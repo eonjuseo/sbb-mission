634196b0551344937f295467466976f5016b73cb
ejooss@chungbuk.ac.kr <ejooss@chungbuk.ac.kr>
2023-11-28 18:00:53 +0900

    [기능 개발] 3-06

---
+++ src/main/java/com/mysite/sbbmission/user/UserService.java
@@ -0,0 +1,22 @@
+package com.mysite.sbbmission.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder; //빈으로 등록한 PasswordEncoder 객체를 주입
+
+    public SiteUser create(String username, String email, String password) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+    }
+}
