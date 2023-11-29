ad40f782522f3ef59c68a28ed621453dc4476e21
ejooss@chungbuk.ac.kr <ejooss@chungbuk.ac.kr>
2023-11-29 18:07:33 +0900

    [기능 개발] 3-11

--- src/main/java/com/mysite/sbbmission/question/Question.java
+++ src/main/java/com/mysite/sbbmission/question/Question.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -32,4 +33,7 @@ public class Question {
 
     @ManyToOne
     private SiteUser author; //여러개의 질문이 한 명의 사용자에게 작성
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
