41d5cababaddc20ddb30c877303f47d3f35389f9
ejooss@chungbuk.ac.kr <ejooss@chungbuk.ac.kr>
2023-11-29 16:31:54 +0900

    [기능 개발] 3-07

--- src/main/java/com/mysite/sbbmission/user/UserController.java
+++ src/main/java/com/mysite/sbbmission/user/UserController.java
@@ -45,7 +45,12 @@ public class UserController {
             bindingResult.reject("signupFailed", e.getMessage());
             return "signup_form";
         }
-        
+
         return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
     }
 }
